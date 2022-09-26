f8ddd58e093b54a68662abf4041880c6313f2cac
Adugna Tadesse <adugtad@gmail.com>
2022-09-26 11:38:37 +0300

    hangman

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import WrongLetters from "./components/WrongLetters";
+import Popup from "./components/Popup";
+import Notification from "./components/Notifications";
+import { showNotification as show } from "./helpers/helpers";
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+  const [selectedWord, setSelectedWord] = useState("");
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable, selectedWord]);
+
+  useEffect(() => {
+    getWord();
+  }, []);
+
+  const playAgain = () => {
+    setSelectedWord("");
+    setPlayable(true);
+    setCorrectLetters([]);
+    setWrongLetters([]);
+    getWord();
+  };
+
+  const getWord = () => {
+    axios
+      .get("https://random-word-api.herokuapp.com/word")
+      .then((res) => setSelectedWord(res.data[0]));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {selectedWord !== "" ? (
+        <>
+          <Header />
+          <div className="game-container">
+            <Figure wrongLetters={wrongLetters} />
+            <WrongLetters wrongLetters={wrongLetters} />
+            <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+          </div>
+          <Popup
+            correctLetters={correctLetters}
+            wrongLetters={wrongLetters}
+            selectedWord={selectedWord}
+            setPlayable={setPlayable}
+            playAgain={playAgain}
+          />
+          <Notification showNotification={showNotification} />
+        </>
+      ) : (
+        <div></div>
+      )}
+    </>
   );
 }
 
